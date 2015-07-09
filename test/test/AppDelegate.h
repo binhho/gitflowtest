8cf82af3242aa99e604e0047293f0f87f83dede1
Ho Nguyen Binh <binh.ho@pyramid-consulting.com>
2015-07-09 11:24:04 +0700

    *test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Ho Nguyen Binh on 7/9/15.
+//  Copyright (c) 2015 Ho Nguyen Binh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Ho Nguyen Binh on 7/9/15.
+//  Copyright (c) 2015 Ho Nguyen Binh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
